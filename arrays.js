371279cecf387e72d8fceeaff71950085ae30a79
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-19 05:05:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,11 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+<<<<<<< HEAD
   return array
+=======
+  console.log(array)
+>>>>>>> 438f1452f862f9925d4629fc44dea4a1a89065d8
 }
 
 function addElementToEndOfArray (array, element) {
@@ -15,6 +19,7 @@ function addElementToEndOfArray (array, element) {
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
+<<<<<<< HEAD
   return array
 }
 
@@ -23,6 +28,16 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+=======
+  console.log(array)
+}
+
+function accessElementInArray(array, index) {
+  console.log(array[index])
+}
+
+function destructivelyRemoveElementFromBeinginningOfArray(array) {
+>>>>>>> 438f1452f862f9925d4629fc44dea4a1a89065d8
   array.shift()
   return array
 }
